e80c80a0856bb2b1928ebff3e81d7f3540597d97
SkyShadowMeadow <catherine.semiletova@gmail.com>
2021-11-09 10:52:23 +0300

    New sprites are created and added.

---
+++ Assets/Scripts/ParallaxEffect/ParallaxBackground.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ParallaxBackground : MonoBehaviour
+{
+    public ParallaxCamera parallaxCamera;
+    List<ParallaxLayer> parallaxLayers = new List<ParallaxLayer>();
+
+    void Start()
+    {
+        if (parallaxCamera == null)
+            parallaxCamera = Camera.main.GetComponent<ParallaxCamera>();
+        if (parallaxCamera != null)
+            parallaxCamera.onCameraTranslate += Move;
+        SetLayers();
+    }
+
+    void SetLayers()
+    {
+        parallaxLayers.Clear();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform BigLayer = transform.GetChild(i);
+            for (int j = 0; j < transform.childCount; j++)
+            {
+                ParallaxLayer layer = BigLayer.GetChild(j).GetComponent<ParallaxLayer>();
+                Debug.Log(layer);
+
+
+                if (layer != null)
+                {
+                    layer.name = "Layer-" + i;
+                    parallaxLayers.Add(layer);
+                }
+            }
+        }
+    }
+    void Move(float delta)
+    {
+        foreach (ParallaxLayer layer in parallaxLayers)
+        {
+            layer.Move(delta);
+        }
+    }
+}
