6ca8535e53ec87da5cd843bf68b8c2364b13d433
SkyShadowMeadow <catherine.semiletova@gmail.com>
2022-02-13 15:29:01 +0300

    Birds chirping is implemented.

---
+++ Assets/Scripts/Environment/BirdsChirping.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Environment
+{
+    public class BirdsChirping : MonoBehaviour
+    {
+        [SerializeField] private AudioClip _chirping;
+        private AudioSource _audioSource;
+        
+        private float _fadeTimeClip = 2f;
+        private float _speedToFadeClip = 1.5f;
+        private float _startedVolume;
+        private float _timePassed = 0;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _startedVolume = _audioSource.volume;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.TryGetComponent(out Player player))
+            {
+                StartChirping();
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.TryGetComponent(out Player player))
+            {
+                StartCoroutine(FadeOutChirping());
+            }
+        }
+
+        private IEnumerator FadeOutChirping()
+        {
+            while (_timePassed < _fadeTimeClip)
+            {
+                _timePassed += Time.deltaTime;
+                _audioSource.volume = Mathf.Lerp(_audioSource.volume, 0, Time.deltaTime * _speedToFadeClip);
+                yield return null;
+            }
+            _timePassed = 0;
+            _audioSource.Stop();
+        }
+
+        private void StartChirping()
+        {
+            _audioSource.volume = _startedVolume;
+            _audioSource.PlayOneShot(_chirping);
+        }
+    }
+}
